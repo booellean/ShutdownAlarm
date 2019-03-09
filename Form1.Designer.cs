19a133ea379484cc74973d724b486b0994fb5c78
booellean <booelle.commissions@gmail.com>
2019-03-09 09:28:34 -0800

    reset: Resetting behaviors. Clearly a better way to do borderless forms

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -30,7 +30,6 @@ namespace ShutdownAlarmApp
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
             this.shutdown_button = new System.Windows.Forms.Button();
             this.alarm_button = new System.Windows.Forms.Button();
             this.menu_panel = new System.Windows.Forms.Panel();
@@ -39,7 +38,6 @@ namespace ShutdownAlarmApp
             this.textBoxStatic = new System.Windows.Forms.TextBox();
             this.container_panel = new System.Windows.Forms.Panel();
             this.alarm_panel = new System.Windows.Forms.Panel();
-            this.loopTimer = new System.Windows.Forms.Timer(this.components);
             this.menu_panel.SuspendLayout();
             this.shutdown_panel.SuspendLayout();
             this.SuspendLayout();
@@ -134,11 +132,6 @@ namespace ShutdownAlarmApp
             this.alarm_panel.Size = new System.Drawing.Size(370, 463);
             this.alarm_panel.TabIndex = 3;
             // 
-            // loopTimer
-            // 
-            this.loopTimer.Interval = 10;
-            this.loopTimer.Tick += new System.EventHandler(this.InitiateMoveAndResizeEvents);
-            // 
             // ShutdownAlarm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -176,7 +169,6 @@ namespace ShutdownAlarmApp
         private System.Windows.Forms.Panel alarm_panel;
         private System.Windows.Forms.TextBox textBoxStatic;
         private System.Windows.Forms.TextBox textBoxDynamic;
-        private System.Windows.Forms.Timer loopTimer;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -12,7 +12,7 @@ namespace ShutdownAlarmApp
 {
     public partial class ShutdownAlarm : Form
     {
-        private int amount = 10;
+        const int AMOUNT = 10;
         private int diffX;
         private int diffY;
         private int yPos;
@@ -48,178 +48,160 @@ namespace ShutdownAlarmApp
         private void ChangeMouseCursor(object sender, MouseEventArgs e)
         {
 
-            //Top Left Corner and Bottom Right Corner of Document
-            if ((this.IsLeft(e.X) && this.IsTop(e.Y)) || (this.IsRight(e.X) && IsBottom(e.Y)))
-            {
-                Cursor.Current = Cursors.SizeNWSE;
-            }
-            //Top and Bottom of Document
-            if ((this.IsMiddleX(e.X) && (this.IsTop(e.Y))) || (this.IsMiddleX(e.X) && (this.IsBottom(e.Y))))
-            {
-                Cursor.Current = Cursors.SizeNS;
-                //if (e.Button == MouseButtons.Left)
-                //{
-                    //this.diffY = Cursor.Position.Y - somethingElse;
-                    //this.Height = this.Height + this.diffY;
-                    //this.Top = Cursor.Position.Y;
-                //}
+            ////Top Left Corner and Bottom Right Corner of Document
+            //if ((this.IsLeft(e.X) && this.IsTop(e.Y)) || (this.IsRight(e.X) && IsBottom(e.Y)))
+            //{
+            //    Cursor.Current = Cursors.SizeNWSE;
+            //}
+            ////Top and Bottom of Document
+            //if ((this.IsMiddleX(e.X) && (this.IsTop(e.Y))) || (this.IsMiddleX(e.X) && (this.IsBottom(e.Y))))
+            //{
+            //    Cursor.Current = Cursors.SizeNS;
+            //    //if (e.Button == MouseButtons.Left)
+            //    //{
+            //        //this.diffY = Cursor.Position.Y - somethingElse;
+            //        //this.Height = this.Height + this.diffY;
+            //        //this.Top = Cursor.Position.Y;
+            //    //}
 
-                if (e.Button == MouseButtons.Left)
-                {
-                    //this.diffY = Cursor.Position.Y - this.Top;
-                    this.textBoxDynamic.Text = string.Format("{0}", sender);
-                    //this.Height = this.Height + this.diffY;
-                }
-            }
-            //Sides of Document
-            if ((this.IsRight(e.X) && this.IsMiddleY(e.Y)) || ((this.IsLeft(e.X) && this.IsMiddleY(e.Y))))
-            {
-                Cursor.Current = Cursors.SizeWE;
-            }
-            //Top Right Corner and Bottom Left Corner of Document
-            if ((this.IsRight(e.X) && this.IsTop(e.Y)) || (this.IsLeft(e.X) && IsBottom(e.Y)))
-            {
-                Cursor.Current = Cursors.SizeNESW;
-            }
+            //    if (e.Button == MouseButtons.Left)
+            //    {
+            //        //this.diffY = Cursor.Position.Y - this.Top;
+            //        this.textBoxDynamic.Text = string.Format("{0}", sender);
+            //        //this.Height = this.Height + this.diffY;
+            //    }
+            //}
+            ////Sides of Document
+            //if ((this.IsRight(e.X) && this.IsMiddleY(e.Y)) || ((this.IsLeft(e.X) && this.IsMiddleY(e.Y))))
+            //{
+            //    Cursor.Current = Cursors.SizeWE;
+            //}
+            ////Top Right Corner and Bottom Left Corner of Document
+            //if ((this.IsRight(e.X) && this.IsTop(e.Y)) || (this.IsLeft(e.X) && IsBottom(e.Y)))
+            //{
+            //    Cursor.Current = Cursors.SizeNESW;
+            //}
         }
 
         //methods for returning values
 
         private bool IsTop(int y){
-            if (y >= 0 && y < amount) return true;
+            if (y >= 0 && y < AMOUNT) return true;
 
             return false;
         }
 
         private bool IsBottom(int y)
         {
-            if (y >= (this.Height - amount) && y <= this.Height) return true;
+            if (y >= (this.Height - AMOUNT) && y <= this.Height) return true;
 
             return false;
         }
 
         private bool IsLeft(int x)
         {
-            if (x >= 0 && x < amount) return true;
+            if (x >= 0 && x < AMOUNT) return true;
 
             return false;
         }
 
         private bool IsRight(int x)
         {
-            if (x >= (this.Width - amount) && x <= this.Width) return true;
+            if (x >= (this.Width - AMOUNT) && x <= this.Width) return true;
 
             return false;
         }
         private bool IsMiddleX(int x)
         {
-            if (x > amount && x < (this.Width - amount)) return true;
+            if (x > AMOUNT && x < (this.Width - AMOUNT)) return true;
 
             return false;
         }
         private bool IsMiddleY(int y)
         {
-            if (y >= amount && y <= (this.Height - amount)) return true;
+            if (y >= AMOUNT && y <= (this.Height - AMOUNT)) return true;
 
             return false;
         }
 
-        private void MoveOrResizeContainer(object sender, MouseEventArgs e)
-        {
-            //Top and Bottom of Document
-            //if ((this.IsMiddleX(e.X) && (this.IsTop(e.Y))) || (this.IsMiddleX(e.X) && (this.IsBottom(e.Y))))
-            //{
-            //while (this.mouseDown == true)
-            //{
-            //}
-            //if (e.Button == MouseButtons.Left)
-            //{
-            //this.diffY = Cursor.Position.Y - somethingElse;
-
-
-                //}
-                //}
-        }
-        //Start getting the timer to work
-
         private void InitiateMoveAndResizeEvents(object sender, EventArgs e)
         {   
-            this.textBoxDynamic.Text = string.Format("X:{0}, Y:{1}, PrevX: {2}, PrevY: {3}", (Cursor.Position.X - this.xPos), (Cursor.Position.Y - this.yPos), Cursor.Position.X, Cursor.Position.Y);
+        //    this.textBoxDynamic.Text = string.Format("X:{0}, Y:{1}, PrevX: {2}, PrevY: {3}", (Cursor.Position.X - this.xPos), (Cursor.Position.Y - this.yPos), Cursor.Position.X, Cursor.Position.Y);
 
-            //Move only if mouse click position is in upper bar
-            if(IsMiddleX(this.xRelPos) && (this.yRelPos >= amount && this.yRelPos < 23)){
-                Cursor.Current = Cursors.Arrow;
-                this.SetDesktopLocation(this.Location.X + (Cursor.Position.X - this.xPos), this.Location.Y + (Cursor.Position.Y - this.yPos));
-            }
-            else
-            {
-                //Top Left Corner
-                if ((this.IsLeft(this.xRelPos) && this.IsTop(this.yRelPos)))
-                {
-                    Cursor.Current = Cursors.SizeNWSE;
-                    this.Height = this.Height - (Cursor.Position.Y - this.yPos);
-                    this.Width = this.Width - (Cursor.Position.X - this.xPos);
-                    this.SetDesktopLocation(this.Location.X + (Cursor.Position.X - this.xPos), this.Location.Y + (Cursor.Position.Y - this.yPos));
-                }
-                //Bottom Right Corner of Document
-                if ((this.IsRight(this.xRelPos) && IsBottom(this.yRelPos)))
-                {
-                    Cursor.Current = Cursors.SizeNWSE;
-                    this.Height = this.Height + (Cursor.Position.Y - this.yPos);
-                    this.Width = this.Width + (Cursor.Position.X - this.xPos);
-                }
-                //Top of Document
-                if ((this.IsMiddleX(this.xRelPos) && (this.IsTop(this.yRelPos))))
-                {
-                    Cursor.Current = Cursors.SizeNS;
-                }
-                //Bottom of Document
-                if ((this.IsMiddleX(this.xRelPos) && (this.IsBottom(this.yRelPos))))
-                {
-                    Cursor.Current = Cursors.SizeNS;
-                }
-                //Right Side of Document
-                if ((this.IsRight(this.xRelPos) && this.IsMiddleY(this.yRelPos)))
-                {
-                    Cursor.Current = Cursors.SizeWE;
-                }
-                //Left Side of Document
-                if (((this.IsLeft(this.xRelPos) && this.IsMiddleY(this.yRelPos))))
-                {
-                    Cursor.Current = Cursors.SizeWE;
-                }
-                //Top Right Corner of Document
-                if ((this.IsRight(this.xRelPos) && this.IsTop(this.yRelPos)))
-                {
-                    Cursor.Current = Cursors.SizeNESW;
-                }
-                //Bottom Left Corner of Document
-                if ((this.IsLeft(this.xRelPos) && IsBottom(this.yRelPos)))
-                {
-                    Cursor.Current = Cursors.SizeNESW;
-                }
-            }
+        //    //Move only if mouse click position is in upper bar
+        //    if(IsMiddleX(this.xRelPos) && (this.yRelPos >= AMOUNT && this.yRelPos < 23)){
+        //        Cursor.Current = Cursors.Arrow;
+        //        this.SetDesktopLocation(this.Location.X + (Cursor.Position.X - this.xPos), this.Location.Y + (Cursor.Position.Y - this.yPos));
+        //    }
+        //    else
+        //    {
+        //        //Top Left Corner
+        //        if ((this.IsLeft(this.xRelPos) && this.IsTop(this.yRelPos)))
+        //        {
+        //            Cursor.Current = Cursors.SizeNWSE;
+        //            this.Height = this.Height - (Cursor.Position.Y - this.yPos);
+        //            this.Width = this.Width - (Cursor.Position.X - this.xPos);
+        //            this.SetDesktopLocation(this.Location.X + (Cursor.Position.X - this.xPos), this.Location.Y + (Cursor.Position.Y - this.yPos));
+        //        }
+        //        //Bottom Right Corner of Document
+        //        if ((this.IsRight(this.xRelPos) && IsBottom(this.yRelPos)))
+        //        {
+        //            Cursor.Current = Cursors.SizeNWSE;
+        //            this.Height = this.Height + (Cursor.Position.Y - this.yPos);
+        //            this.Width = this.Width + (Cursor.Position.X - this.xPos);
+        //        }
+        //        //Top of Document
+        //        if ((this.IsMiddleX(this.xRelPos) && (this.IsTop(this.yRelPos))))
+        //        {
+        //            Cursor.Current = Cursors.SizeNS;
+        //        }
+        //        //Bottom of Document
+        //        if ((this.IsMiddleX(this.xRelPos) && (this.IsBottom(this.yRelPos))))
+        //        {
+        //            Cursor.Current = Cursors.SizeNS;
+        //        }
+        //        //Right Side of Document
+        //        if ((this.IsRight(this.xRelPos) && this.IsMiddleY(this.yRelPos)))
+        //        {
+        //            Cursor.Current = Cursors.SizeWE;
+        //        }
+        //        //Left Side of Document
+        //        if (((this.IsLeft(this.xRelPos) && this.IsMiddleY(this.yRelPos))))
+        //        {
+        //            Cursor.Current = Cursors.SizeWE;
+        //        }
+        //        //Top Right Corner of Document
+        //        if ((this.IsRight(this.xRelPos) && this.IsTop(this.yRelPos)))
+        //        {
+        //            Cursor.Current = Cursors.SizeNESW;
+        //        }
+        //        //Bottom Left Corner of Document
+        //        if ((this.IsLeft(this.xRelPos) && IsBottom(this.yRelPos)))
+        //        {
+        //            Cursor.Current = Cursors.SizeNESW;
+        //        }
+        //    }
 
-            //Reset the previous coordinates at end after changes
-            this.xPos = Cursor.Position.X;
-            this.yPos = Cursor.Position.Y;
+        //    //Reset the previous coordinates at end after changes
+        //    this.xPos = Cursor.Position.X;
+        //    this.yPos = Cursor.Position.Y;
         }
         private void MouseDownChange(object sender, MouseEventArgs e)
         {
-            //Set the previous coordinates before the timer starts
-            this.xPos = Cursor.Position.X;
-            this.yPos = Cursor.Position.Y;
-            //Position of mouse relative to Panels
-            this.xRelPos = e.Location.X;
-            this.yRelPos = e.Location.Y;
-            //start the timer
-            loopTimer.Enabled = true;
-            loopTimer.Start();
+        //    //Set the previous coordinates before the timer starts
+        //    this.xPos = Cursor.Position.X;
+        //    this.yPos = Cursor.Position.Y;
+        //    //Position of mouse relative to Panels
+        //    this.xRelPos = e.Location.X;
+        //    this.yRelPos = e.Location.Y;
+        //    //start the timer
+        //    loopTimer.Enabled = true;
+        //    loopTimer.Start();
         }
         private void MouseUpChange(object sender, MouseEventArgs e)
         {
-            loopTimer.Stop();
-            this.textBoxDynamic.Text = string.Format("Loop Timer Just stopped!");
+        //    loopTimer.Stop();
+        //    this.textBoxDynamic.Text = string.Format("Loop Timer Just stopped!");
         }
     }
 }
